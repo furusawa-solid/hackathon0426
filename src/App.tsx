c43588d0955c29ec78126bfe6a968baf55270471
levin256 <levin256@gmail.com>
2025-04-26 13:06:28 +0900

    fix: 個別のコードを削除して汎用的なベースプロジェクトになるよう修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,251 +1,69 @@
-import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faUpload } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useEffect, useRef, useState } from 'react';
+import { Button, Field, Input, Textarea } from '@headlessui/react';
 import './App.css';
-import { v4 as uuidv4 } from 'uuid';
-import { HowToUse } from './HowToUse';
-import {
-  type IgnoreLinePrefix,
-  type IgnoreString,
-  validateIgnoreLinePrefix,
-  validateIgnoreString,
-} from './schemas';
-import { escapeRegExp } from './utils/string';
+import ApiKeyDialog from './components/ApiKeyDialog';
+import { IconButton } from './components/IconButtom';
 
 const App = () => {
-  const excludeInogreString = (text: string) => {
-    const textWithoutIgnoreLine = ignoreLinePrefixes.reduce(
-      (currentText, { ignoreLinePrefix }) => {
-        const lines = currentText.split(/\n/);
-        return lines
-          .filter((line) => !line.startsWith(ignoreLinePrefix))
-          .join('\n');
-      },
-      text,
-    );
+  const onFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+    const files = e.target.files;
 
-    const currentIgnoreStrings = [
-      ...ignoreStrings.map(({ ignoreString }) => ignoreString),
-    ];
-    if (isIgnoreSpace) {
-      currentIgnoreStrings.push(' ');
-      currentIgnoreStrings.push('\u3000');
-    }
-    if (isIgnoreLineBreak) {
-      currentIgnoreStrings.push('\n');
-    }
-    const textWithoutIgnoreString = currentIgnoreStrings.reduce(
-      (currentText, string) => {
-        const escapeString = escapeRegExp(string);
-        const regex = new RegExp(escapeString, 'g');
-        return currentText.replaceAll(regex, '');
-      },
-      textWithoutIgnoreLine,
-    );
-    return textWithoutIgnoreString;
-  };
-
-  const ignoreStringRef = useRef<HTMLInputElement>(null!);
-  const ignoreLinePrefixRef = useRef<HTMLInputElement>(null!);
-  const [isIgnoreSpace, setIsIgnoreSpace] = useState<boolean>(false);
-  const [isIgnoreLineBreak, setIsIgnoreLineBreak] = useState<boolean>(false);
-  const [ignoreStrings, setIgnoreStrings] = useState<IgnoreString[]>(
-    ['、', '。', '「', '」', '…', '！', '？'].map((ignoreString) => ({
-      id: uuidv4(),
-      ignoreString,
-    })),
-  );
-  const [ignoreLinePrefixes, setIgnoreLinePrefixes] = useState<
-    IgnoreLinePrefix[]
-  >(['//'].map((ignoreLinePrefix) => ({ id: uuidv4(), ignoreLinePrefix })));
-  const [text, setText] = useState('');
-  const [textCount, setTextCount] = useState(excludeInogreString(text).length);
-
-  useEffect(() => {
-    const ignoreStringsJson = localStorage.getItem('ignoreStrings');
-    if (ignoreStringsJson) {
-      setIgnoreStrings(JSON.parse(ignoreStringsJson));
-    }
-
-    const ignoreLinePrefixesJson = localStorage.getItem('ignoreLinePrefixes');
-    if (ignoreLinePrefixesJson) {
-      setIgnoreLinePrefixes(JSON.parse(ignoreLinePrefixesJson));
-    }
-
-    const handleBeforeUnload = (event: Event) => {
-      event.preventDefault();
+    reader.onload = () => {
+      const fileData = reader.result;
+      if (fileData) {
+        // textRef.current.value = fileData.toString();
+      }
     };
-    window.addEventListener('beforeunload', handleBeforeUnload);
-    return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
-    };
-  }, []);
-
-  useEffect(() => {
-    setTextCount(excludeInogreString(text).length);
-  }, [
-    text,
-    isIgnoreSpace,
-    isIgnoreLineBreak,
-    ignoreStrings,
-    ignoreLinePrefixes,
-  ]);
-
-  const addIgnoreString = () => {
-    const inputIgnoreString = {
-      id: uuidv4(),
-      ignoreString: ignoreStringRef.current?.value,
-    };
-    const result = validateIgnoreString(inputIgnoreString);
-    if (!result.success) {
-      alert(result.messages);
-      return;
+    if (files) {
+      reader.readAsText(files[0]);
     }
-
-    const newIgnoreStrings = [...ignoreStrings, inputIgnoreString];
-    setIgnoreStrings(newIgnoreStrings);
-    localStorage.setItem('ignoreStrings', JSON.stringify(newIgnoreStrings));
-    ignoreStringRef.current.value = '';
-  };
-
-  const addIgnoreLinePrefix = () => {
-    const inputIgnoreLinePrefix = {
-      id: uuidv4(),
-      ignoreLinePrefix: ignoreLinePrefixRef.current?.value,
-    };
-    const result = validateIgnoreLinePrefix(inputIgnoreLinePrefix);
-    if (!result.success) {
-      alert(result.messages);
-      return;
-    }
-    const newIgnoreLinePrefixes = [
-      ...ignoreLinePrefixes,
-      inputIgnoreLinePrefix,
-    ];
-    setIgnoreLinePrefixes(newIgnoreLinePrefixes);
-    localStorage.setItem(
-      'ignoreLinePrefixes',
-      JSON.stringify(newIgnoreLinePrefixes),
-    );
-    ignoreLinePrefixRef.current.value = '';
-  };
-
-  const deleteIgnoreString = (deleteIgnoreStringId: string) => {
-    const afterDeleteIgnoreStrings = ignoreStrings.filter(
-      ({ id }) => id !== deleteIgnoreStringId,
-    );
-    setIgnoreStrings(afterDeleteIgnoreStrings);
-    localStorage.setItem(
-      'ignoreStrings',
-      JSON.stringify(afterDeleteIgnoreStrings),
-    );
-  };
-
-  const deleteIgnoreLinePrefix = (deleteIgnoreLinePrefixId: string) => {
-    const afterDeleteIgnoreLinePrefixes = ignoreLinePrefixes.filter(
-      ({ id }) => id !== deleteIgnoreLinePrefixId,
-    );
-    setIgnoreLinePrefixes(afterDeleteIgnoreLinePrefixes);
-    localStorage.setItem(
-      'ignoreLinePrefixes',
-      JSON.stringify(afterDeleteIgnoreLinePrefixes),
-    );
   };
 
   return (
-    <div className="gap05rem flex-column">
-      <h1>台詞カウンター</h1>
-      <HowToUse />
-      <fieldset className="display-flex gap1rem">
-        <legend>特殊文字のカウント設定</legend>
-        <label>
-          <input
-            type="checkbox"
-            checked={isIgnoreSpace}
-            onChange={(event) => setIsIgnoreSpace(event.target.checked)}
-          />
-          空白をカウントしない
-        </label>
-        <label>
-          <input
-            type="checkbox"
-            checked={isIgnoreLineBreak}
-            onChange={(event) => setIsIgnoreLineBreak(event.target.checked)}
-          />
-          改行をカウントしない
-        </label>
-      </fieldset>
-      <fieldset>
-        <legend>カウントしない文字</legend>
-        <ul>
-          {ignoreStrings.map(({ id, ignoreString }) => (
-            <li key={id}>
-              <div className="display-flex padding-block">
-                <code className="code-label">{ignoreString}</code>
-                <button
-                  type="button"
-                  className="trash-btn"
-                  onClick={() => deleteIgnoreString(id)}
-                >
-                  <FontAwesomeIcon icon={faTrash} />
-                </button>
-              </div>
+    <div className="flex flex-col justify-start gap-y-3 p-4">
+      <h1>Title is here.</h1>
+      <fieldset className="px-3 pt-1 pb-3">
+        <legend>Legend</legend>
+        {/* <ul className="flex flex-col gap-y-2">
+          {[].map((config, index) => (
+            <li key={index} className="flex w-60">
+              <code className="flex-1 overflow-hidden text-ellipsis whitespace-nowrap rounded-s-md bg-neutral-700 px-2 py-1 text-left text-sm underline decoration-neutral-400 underline-offset-4">
+                {config}
+              </code>
+              <Button className="rounded-e-md bg-red-800" onClick={() => {}}>
+                <FontAwesomeIcon icon={faTrash} />
+              </Button>
             </li>
           ))}
-        </ul>
-        <div className="btn-container">
+        </ul> */}
+        <div className="flex w-60 pt-5">
           <input
             type="text"
-            placeholder="Enter ignore string."
-            ref={ignoreStringRef}
+            className="box-border flex-1 rounded-s-md border-none bg-neutral-600 px-2 py-1 outline-none"
+            placeholder="Placeholder is here."
           />
-          <button type="button" onClick={addIgnoreString}>
+          <Button onClick={() => {}} className="rounded-e-md bg-teal-700">
             <FontAwesomeIcon icon={faPlus} />
-          </button>
+          </Button>
         </div>
       </fieldset>
-      <fieldset>
-        <legend>カウントしない行</legend>
-        <ul>
-          {ignoreLinePrefixes.map(({ id, ignoreLinePrefix }) => (
-            <li key={id}>
-              <div className="display-flex padding-block">
-                <code className="code-label">{ignoreLinePrefix}</code>
-                <button
-                  type="button"
-                  className="trash-btn"
-                  onClick={() => deleteIgnoreLinePrefix(id)}
-                >
-                  <FontAwesomeIcon icon={faTrash} />
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <div className="btn-container">
-          <input
-            type="text"
-            placeholder="Enter ignore line."
-            ref={ignoreLinePrefixRef}
-          />
-          <button
-            type="button"
-            className="add-btn"
-            onClick={addIgnoreLinePrefix}
-          >
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
-        </div>
-      </fieldset>
-      <div className="flex-column">
-        <h3>テキスト（文字数: {textCount}）</h3>
-        <textarea
-          className="radius-6p height-500 flex-column"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="Enter text."
+      <Field className="flex flex-col">
+        <h2>TextArea</h2>
+        <Textarea
+          className="h-[32rem] rounded-md border-none bg-neutral-600 p-3 outline-none"
+          defaultValue="デフォルトテキスト"
+          placeholder="Placeholder is here."
         />
-      </div>
+      </Field>
+      <Input className="hidden" type="file" onChange={onFileChange} />
+      <IconButton
+        label="ファイルを読み込む"
+        icon={faUpload}
+        onClick={() => {}}
+      />
+      <ApiKeyDialog isOpen={false} setIsOpen={() => {}} />
     </div>
   );
 };
